b824ee8a28f2ed1bf7f41a308b4c37fb39344ac3
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2022-02-14 10:05:06 +0900

    [Bronze IV] Title: 윤년, Time: 108 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Bronze/2753.윤년/윤년.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int year = scanner.nextInt();
+	int judge=0;
+	if(((year%4==0)&&(year%100!=0))||(year%400==0)) {
+		judge = 1;
+	}else {judge = 0;}
+
+	System.out.println(judge);
+}
+	}
