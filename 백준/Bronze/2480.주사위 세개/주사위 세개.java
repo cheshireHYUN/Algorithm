a13c92386d646f3c8d10ff5201e83b0e3c3157ac
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2022-02-15 21:29:56 +0900

    [Bronze IV] Title: 주사위 세개, Time: 108 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/2480.주사위 세개/주사위 세개.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int dice1 = scanner.nextInt();
+		int dice2 = scanner.nextInt();
+		int dice3 = scanner.nextInt();
+		int result = 0;
+
+		if (dice1 == dice2 && dice1 == dice3 && dice2 == dice3) {
+			result = 10000 + dice2 * 1000;
+		}
+		else if (dice1 == dice2 || dice1 == dice3) {
+			result = 1000+dice1*100;
+		}
+		else if (dice2==dice3) {
+			result = 1000+dice2*100;
+		}
+		else {
+			result = Math.max(dice3, Math.max(dice1, dice2))*100;
+		}
+		
+		System.out.println(result);
+
+	}
+}
