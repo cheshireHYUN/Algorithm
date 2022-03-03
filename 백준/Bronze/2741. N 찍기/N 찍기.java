b4050bcd0f27a84537f5c49483cb5d56932530b5
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2022-03-03 11:07:05 +0900

    [Bronze III] Title: N 찍기, Time: 1524 ms, Memory: 32436 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int number = scanner.nextInt();
+		
+		for(int i=1;i<=number;i++) {
+			System.out.println(i);
+		}
+	
+	
+	
+	}
+	
+
+}
