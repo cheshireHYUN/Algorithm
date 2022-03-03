dfc317bdb2c63dd8b8a3be84cfb493e1c0865ac1
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2022-03-03 11:06:49 +0900

    [Bronze III] Title: 기찍 N, Time: 1580 ms, Memory: 38488 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 기찍 N - 2742 
+
+[문제 링크](https://www.acmicpc.net/problem/2742) 
+
+### 성능 요약
+
+메모리: 38488 KB, 시간: 1580 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		for(int i=N;i>0;i--) {
+			System.out.println(i);
+		}
+	
+	
+	
+	}
+	
+
+}
