f9e4e44ac21a1f6462cdadd85618bcf9d958bc56
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-03-19 18:23:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.15 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,45 +1,22 @@
 import java.util.*;
 class Solution {
-	// n의 약수를 구하는 메소드
-	public HashSet<Integer> solv2(int n){
-		HashSet<Integer> answer = new HashSet<>();
-        for(int i=1; i<=n; i++){
-            int tmp = 0;
-            int j=1;
-            while(tmp < n){
-                tmp = i*j;
-                if(tmp==n){
-                	answer.add(i);
-                	answer.add(j);
-                    break;
-                }
-                j++;
-            }
-        }
-		return answer;
-	}
 	
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int n = numer1*denom2+numer2*denom1;
+        int n = numer1*denom2 + numer2*denom1;
         int d = denom1*denom2;
         
-        // 각 공약수 구하기
-        HashSet<Integer> nSet = solv2(n);
-        HashSet<Integer> dSet = solv2(d);
-        
-        //최대공약수 찾기
-        int max = 1;
-        for(int i : nSet) {
-        	for(int j : dSet) {
-        		if( i==j ) max = Math.max(max, j);
-        	}
+        //최대공약수를 찾는법) 더 작은수를 1부터 나눠보면서 더 큰수에서도 나눠지는 수를 찾으면 됨
+        int min = Math.min(n,d);
+        int max = Math.max(n,d);
+        int common = 1;
+        for(int i=1; i<=min; i++){
+            if(min%i==0 && max%i==0){
+                common = i;
+            }
         }
-
-        //최대공약수로 약분하기
-        n /= max;
-        d /= max;
         
-        int[] answer = {n,d};
+        //약분하고 리턴
+        int[] answer = {n/common,d/common};
         return answer;
     }
 }
