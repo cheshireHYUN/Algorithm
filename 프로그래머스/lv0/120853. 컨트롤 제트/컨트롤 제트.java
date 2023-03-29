d6c08184deec78e826ab741c8b759c7a49647bfd
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-03-30 04:03:43 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.29 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,15 +1,20 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
         //숫자더하기 할건데 Z가 나오면 바로전에 더한거 빼는거임.
         int answer = 0;
         String[] strArr = s.split(" ");
+        Stack<Integer> stack = new Stack<>();
         for(int i=0; i<strArr.length; i++){
             if(strArr[i].equals("Z")){
-                answer -= Integer.parseInt(strArr[i-1]);
+                stack.pop();
             }
-            else answer += Integer.parseInt(strArr[i]);
+            else stack.push(Integer.parseInt(strArr[i]));
         }
         
+        for(int i : stack){
+            answer += i;
+        }
         return answer;
     }
 }
