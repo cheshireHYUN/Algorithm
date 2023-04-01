576567f84a4947d4240fda0192ac9c88045ba48d
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-04-02 05:04:02 +0900

    [level 0] Title: 중복된 문자 제거, Time: 8.81 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        char[] arr = my_string.toCharArray();
+        for(char c : arr){
+            if(!answer.contains(String.valueOf(c))) answer+=c;
+        }
+        return answer;
+    }
+}
