78eae8738aebdf2f416fb2e2beff8d46bd91b88a
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-04-02 02:50:27 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n/10!=0){
+        while(n!=0){
             answer += n%10;
             n = n/10;
         }
-        answer += n%10;
         return answer;
     }
 }
