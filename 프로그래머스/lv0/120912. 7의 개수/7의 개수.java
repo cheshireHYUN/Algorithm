ddd839c2e88e40e5511c40bad23c9dbd2be6947b
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-04-02 18:09:28 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -2,12 +2,9 @@ class Solution {
     public int solution(int[] array) {
         int answer = 0;
         for(int a : array){
-            if(Integer.toString(a).contains("7")){
-                int tmp = a;
-                while(tmp > 0){
-                    if(tmp%10 == 7) answer ++;
-                    tmp /= 10;
-                }
+            while(a > 0){
+                if(a%10 == 7) answer ++;
+                a /= 10;
             }
         }
         return answer;
