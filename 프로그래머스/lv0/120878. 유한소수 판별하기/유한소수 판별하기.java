28d26af2433594025ea687dd06355ac8d50cb7b4
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-04-02 19:33:31 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.68 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,12 +1,13 @@
+import java.util.*;
+import java.math.BigInteger;
 class Solution {
     public int solution(int a, int b) {
         // 조건 : 기약분수일때 분모 소인수가 2와 5만 존재한다.
         int answer = 2;
-        // 기약분수 만들기 : 두 정수의 최소공배수로 나누기
-        int max = 1;
-        for(int i=1; i<=a; i++){
-            if(a%i==0 && b%i==0) max=i;
-        }
+        // 기약분수 만들기 : 두 정수의 최소공배수로 나누기 ; 라이브러리 사용하기
+        BigInteger b_a = BigInteger.valueOf(a);
+        BigInteger b_b = BigInteger.valueOf(b);
+        int max = (b_a.gcd(b_b)).intValue();
         
         // 분모의 소인수가 2와 5만 존재하면 유한소수.. 즉 2와 5의 곱으로 표현이 가능해야함
         b /= max;
