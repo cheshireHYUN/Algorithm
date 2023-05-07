42e53a4cb82ab8750f799eee9624fbd5ddb7b8d2
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-05-07 22:46:54 +0900

    [Silver I] Title: 영역 구하기, Time: 140 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -4,28 +4,31 @@ public class Main {
     static int col;
     static int row;
     static int[][] map;
-    static int[][] checkArr;
+    static int[][] check;
     static int[] dx = {0,1,0,-1};
     static int[] dy = {1,0,-1,0};
 
-    private static int bfs(int x, int y){
-        Queue<int[]> Q = new LinkedList<int[]>();
-        Q.offer(new int[] {x,y});
-        checkArr[x][y]=1;
+    public static class Node{
+        public Node(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        int x,y;
+    }
+    private static int BFS(Node node){
+        Queue<Node> Q = new LinkedList<Node>();
+        Q.offer(node);
         int cnt = 1;
-        // 방향벡터 돌면서 다음노드 찾고, 해당되면 큐에 추가해서 큐가 더이상 없을때까지(다음노드 없을때까지) 반복
         while(!Q.isEmpty()){
-            int[] data = Q.poll();
-            int currX = data[0];
-            int currY = data[1];
+            Node data = Q.poll();
             for(int i=0; i<4; i++){
-                int nx = currX+dx[i];
-                int ny = currY+dy[i];
-                if(nx>=0 && nx<row && ny>=0 && ny<col
-                    && checkArr[nx][ny]==0 && map[nx][ny]==0){
-                    Q.offer(new int[] {nx,ny});
-                    checkArr[nx][ny]=1;
+                int nx = data.x+dx[i];
+                int ny = data.y+dy[i];
+                if(nx>=0 && ny>=0 && nx<row && ny<col &&
+                        check[nx][ny]==0 && map[nx][ny]==0){
+                    check[nx][ny] =1;
                     cnt++;
+                    Q.offer(new Node(nx, ny));
                 }
             }
         }
@@ -35,36 +38,34 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         row = sc.nextInt();
         col = sc.nextInt();
-        map = new int[row][col];
-        checkArr = new int[row][col];
         int k = sc.nextInt();
+        map = new int[row][col];
+        check = new int[row][col];
         for(int i=0; i<k; i++){
-            int x1 = sc.nextInt(); //왼쪽아래
+            int x1 = sc.nextInt();
             int y1 = sc.nextInt();
-            int x2 = sc.nextInt(); //오른쪽위
+            int x2 = sc.nextInt(); //두번째로 입력한게 반드시 더 크다
             int y2 = sc.nextInt();
-            //항상 두번째로 입력받은게 더 크다
             for(int x=x1; x<x2; x++){
                 for(int y=y1; y<y2; y++){
-                    //x는 col, y는 row니까
-                    map[y][x] = 1;
+                    map[y][x]= 1; //x값은 열값, y값은 행값이므로 순서가 반대
                 }
             }
         }
-
-        List<Integer> areaList = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if (map[i][j] != 1 && checkArr[i][j] != 1) {
-                    areaList.add(bfs(i,j));
+                if(map[i][j]==0 && check[i][j]==0){
+                    check[i][j] = 1;
+                    list.add(BFS(new Node(i,j)));
                 }
             }
         }
 
-        System.out.println(areaList.size());
-        Collections.sort(areaList);
-        for(int area : areaList){
-            System.out.print(area+" ");
+        System.out.println(list.size());
+        Collections.sort(list);
+        for(int li : list){
+            System.out.print(li+" ");
         }
 
     }
