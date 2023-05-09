b154d4616b50533f5fe1fe113396ce8407f8af78
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-05-10 00:23:55 +0900

    [Silver I] Title: 컴백홈, Time: 116 ms, Memory: 13464 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.java
+++ 백준/Silver/1189. 컴백홈/컴백홈.java
@@ -1,57 +1,49 @@
 import java.util.*;
 
 public class Main {
-
-    static int R,C,K,cnt=0;
-    static int[][] checkedArr;
+    static int row;
+    static int col;
+    static int k;
     static char[][] map;
+    static int[][] check;
     static int[] dx = {0,1,0,-1};
     static int[] dy = {1,0,-1,0};
+    static int cnt = 0;
 
-    static void dfs(int x, int y, int len) {
-        // stop : 도착점(0,C-1)에 도착할경우
-        if(x==0 && y==C-1){
-            if(len == K) cnt++;
+    public static void dfs(int x, int y, int len){
+        if(x==0 && y==col-1){
+            if(len == k) cnt++;
             return;
         }
 
-        // 상하좌우 검사하며 재귀 호출한다
         for(int i=0; i<4; i++){
             int nx = x+dx[i];
             int ny = y+dy[i];
-            // map 범위안에 있고 / T가 아니면서 / 방문한적 없을때만 재귀
-            if(nx>=0 && nx<R && ny>=0 && ny<C
-                    && map[nx][ny] != 'T'
-                    && checkedArr[nx][ny] == 0){
-                // 체크배열 1로 바꾼뒤 재귀
-                checkedArr[nx][ny] = 1;
-                dfs(nx, ny, len+1);
-                // 백트래킹(끝난경로 직전으로 돌아가서 다른경로 탐색)
-                checkedArr[nx][ny] = 0;
+            if(nx>=0 && ny>=0 && nx<row && ny<col && check[nx][ny]==0 && map[nx][ny]!='T'){
+                check[nx][ny] = 1;
+                dfs(nx,ny,len+1);
+                check[nx][ny] = 0;
             }
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        R = sc.nextInt(); //열
-        C = sc.nextInt(); //행
-        K = sc.nextInt(); //길이
-        checkedArr = new int[R][C];
-        map = new char[R][C];
-
-        for(int i=0; i<R; i++) {
-            String tmp = sc.next();
-            for(int j=0; j<C; j++) {
-                map[i][j] = tmp.charAt(j);
+        row = sc.nextInt();
+        col = sc.nextInt();
+        k = sc.nextInt();
+        map = new char[row][col];
+        check = new int[row][col];
+        for(int i=0; i<row; i++){
+            String str = sc.next();
+            for(int j=0; j<col; j++){
+                map[i][j] = str.charAt(j);
             }
         }
 
-        // 시작점(C-1,0) 셋팅
-        checkedArr[R-1][0] = 1;
-        dfs(R-1,0,1);
+        //경로의 가짓수를 찾으므로 dfs한다.
+        check[row-1][0] = 1;
+        dfs(row-1,0,1);
         System.out.println(cnt);
+
     }
-
-
 }
