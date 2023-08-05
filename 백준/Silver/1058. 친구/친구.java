ae6c22beaa83637b571ad3b03f0f6e3536d6a389
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-08-06 00:39:40 +0900

    [Silver II] Title: 친구, Time: 80 ms, Memory: 11756 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -18,48 +18,44 @@ import java.util.StringTokenizer;
  * input : 사람수 N(<=50), N개 줄에 각 사람이 친구이면 Y 아니면 N이 주어진다.
  * output : 가장 유명한 사람의 2-친구수를 출력
  * 
- * 풀이 방법 : 인접리스트로 풀어보려다 실패 ㅎ 검색해보니 플루이드 워샬 사용하래서 그렇게함
- * 플루이드 와샬 = 모든노드에서 모든노드까지의 최단경로를 찾는 알고리즘
- * 따라서 arr에서 1로 표현되어있다면 친구인것이고, 2로 표현되어있다면 경유노드가 하나 있는것임 즉 2-친구
- * 2-친구를 찾으므로 값이 2이하인 arr을 카운트
+ * 풀이 방법 : 완전탐색으로 각 노드의 친구를 카운트
  */
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		int N, arr[][], max=100000000, answer=0;
+		int N, max=100000000, answer=0;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N][N];
+		char[][] arr = new char[N][N];
+		int[] result = new int[N]; //각 노드의 친구수를 저장한다
+		boolean[][] check = new boolean[N][N];
 		
-		// 플루이드 와샬로 풀어보자
-		
-		// 인접노드와의 정보를 arr에 채우기
 		for(int i=0; i<N; i++) {
-			String line = br.readLine()
-;			for(int j=0; j<N; j++) {
-				if(line.charAt(j)=='Y') arr[i][j] = 1;
-				else if(i!=j) arr[i][j]=max; //i=j일땐 0이 맞고, i!=j이면서 친구가 아니면 무한대로 본다
+			String line = br.readLine();
+			for(int j=0; j<N; j++) {
+				arr[i][j] = line.charAt(j);
 			}
 		}
 		
-		for(int k=0; k<N; k++) {
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(i==j || j==k || i==k) continue; //대각선방향과 경유노드가 이미 포함된 노드는 무시
-					else if(arr[i][j]>arr[i][k]+arr[k][j]) {
-						arr[i][j] = arr[i][k]+arr[k][j];
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				if(arr[i][j] == 'Y') { //i의 친구일때
+					if(!check[i][j]) { //이전에 체크한노드도 아닐때
+						result[i]++;
+						check[i][j]=true;
+					}
+					// 친구의 친구가 있는지도 체크해준다. i=본인, j=친구, k=친구의친구
+					for(int k=0; k<N; k++) {
+						if(!check[i][k] && i!=k && arr[j][k]=='Y') {
+							result[i]++;
+							check[i][k]=true;
+						}
 					}
 				}
 			}
 		}
 		
-		for(int i=0; i<N; i++) {
-			int tmp = 0;
-			for(int j=0; j<N; j++) {
-				if(i==j) continue;
-				else if(arr[i][j]<=2) tmp++;
-			}
-			answer = Math.max(answer, tmp);
-		}
-		System.out.println(answer);
+		Arrays.sort(result);
+		System.out.println(result[N-1]);
+
 	}
 }
