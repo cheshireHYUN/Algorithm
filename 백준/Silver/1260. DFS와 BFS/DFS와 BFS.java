006521d746e7a3a59690fed7c68bd26b165fb80a
cheshireHYUN <89832538+cheshireHYUN@users.noreply.github.com>
2023-08-18 16:06:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 240 ms, Memory: 21328 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,74 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 /** DFS와 BFS
- * 방문할 수 있는 정점이 여러개인 경우 작은것부터 방문, 정점번호는 1부터 N까지임
- * input : 정점N개와 간선M개, 탐색 시작번호V
- * output : DFS 탐색결과 \n BFS 탐색결과
+ * 방문할 수 있는 정점이 여러개인경우 번호가 작은것을 먼저 방문하고,
+ * 더이상 방문할 수 있는 점이 없으면 종료
+ * 
+ * 저번엔 인접리스트로 풀었으니 이번엔 인접행렬로 풀어보자..
  */
-
 public class Main {
-	static int[][] arr;
-	static int n,m;
-	static boolean[] check1;
-	static boolean[] check2;
-
-	public static void DFS(int v) {
-		System.out.print(v+" ");
-		for(int i=1; i<=n; i++) {
-			if(arr[v][i]==1 && check1[i]==false) {
-				check1[i] = true;
-				DFS(i);
-			}
+	static int[] dx = {};
+	static int[] dy = {};
+	static int[][] map;
+	static boolean[] check;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()); //정점 갯수
+		int M = Integer.parseInt(st.nextToken()); //간선 갯수
+		int V = Integer.parseInt(st.nextToken()); //시작 정점 번호
+		map = new int[N+1][N+1];
+		check = new boolean[N+1];
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			map[x][y] = 1;
+			map[y][x] = 1;
 		}
+		
+		check[V]=true;
+		dfs(V);
+		sb.append('\n');
+		
+		Arrays.fill(check, false); //체크배열 초기화
+		bfs(V);
+		System.out.println(sb);
 	}
-
-	public static void BFS(int v) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(v);
-		check2[v]=true;
-		while(!q.isEmpty()) {
-			int curr = q.poll();
-			System.out.print(curr+" ");
-			for(int i=1; i<=n; i++) {
-				if(arr[curr][i] == 1 && check2[i] == false) {
-					q.offer(i);
-					check2[i] = true;
-				}
+	
+	// v와 연결된 노드 찾기, 즉 map에서 v행의 값이 1인곳 찾기
+	private static void dfs(int v) {
+		sb.append(v).append(' ');
+		for(int i=1; i<map[0].length; i++) {
+			if(map[v][i]==1 && !check[i]) {
+				check[i] = true;
+				dfs(i);
 			}
 		}
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt(); //정점번호는 1~n
-		m = sc.nextInt(); //간선갯수
-		int v = sc.nextInt(); //탐색시작점
-		
-		arr = new int[n+1][n+1];
-		check1 = new boolean[n+1];	
-		check2 = new boolean[n+1];	
-		for(int i=0; i<m; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			arr[x][y] = 1;
-			arr[y][x] = 1; //무방향 그래프이므로
+	private static void bfs(int v) {
+		check[v]=true;
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(v);
+		while(!q.isEmpty()){
+			int x = q.poll();
+			sb.append(x).append(' ');
+			for(int i=0; i<map[0].length; i++) {
+				if(map[x][i]==1 && !check[i]) {
+					check[i] = true;
+					q.offer(i);
+				}
+			}
 		}
-		check1[v]=true;
-		DFS(v);
-		System.out.println();
-		BFS(v);
-		
 	}
 }
