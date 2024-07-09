7010ee16013b5534bf5e37fd6d35b510124d4d7b
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-10 00:30:34 +0900

    [Gold III] Title: 줄 세우기, Time: 376 ms, Memory: 46520 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,64 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /** 줄세우기
- * 
- * N명 키순서대로 줄세운다. 두 학생의 키를 비교하는 식으로,,
- * N,M이 주어지며 각각 학생의 번호와 키를 비교한 횟수이다.
- * 즉 출석번호는 1~N까지고 M줄에 걸쳐 비교결과가 주어지며 1,3이 주어지면 1이 3보다 크다는 의미이다
+ * N명의 학생을 키순서로 정렬. 두 학생의 키를 비교하며..
+ * 일부학생 키를 비교한 결과가 주어졌을때 줄세우는 프로그램 작성
+ * 정답이 여러개면 그중 아무거나 출력
  *
+ * 풀이 : 방향이있는 그래프이며 사이클이 없으므로 위상정렬 사용 가능.
  */
 public class Main {
-	static int[] numbers;
-	static List<Integer>[] adjList;
-	static int N,M;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken()); //번호
-		M = Integer.parseInt(st.nextToken()); //키잰횟수
-		
-		numbers = new int[N+1]; //1~N까지 학생들이 가지는 앞사람 인원수를 저장. 즉 내앞에 몇명??을 저장
-		adjList = new ArrayList[N+1]; //adjList[1]은 1번학생 이후에 설 수 있는 학생번호를 저장. 즉 내뒤에 누구?를 저장
-		for(int i=1; i<N+1; i++) adjList[i] = new ArrayList<Integer>(); //인접리스트 초기화
-		
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			// 1 3이 주어지면 1이 3번 앞에서야함, 즉 list[1]은 1번 뒤에 설수있는 번호를 저장한다.
-			int front = Integer.parseInt(st.nextToken());
-			int back = Integer.parseInt(st.nextToken());
-			adjList[front].add(back);
-			numbers[back]++;
-		}
-		
-		solution();
-		System.out.println(sb);
-	}
-	
-	
-	private static void solution() {
-		Queue<Integer> q = new ArrayDeque<>();
-		for(int i=1; i<N+1; i++) {
-			if(numbers[i]==0) q.offer(i); //앞에 설 사람 아무도 없는 요소만 큐에 넣는다
-		}
-		while(!q.isEmpty()) {
-			int front = q.poll(); //앞에 설 사람이 없는, 즉 맨앞사람이 될 수 있는 학생을 세운다
-			sb.append(front).append(' ');
-			
-			for(int back : adjList[front]) {//방금 세운 학생의 뒤에 설수있는 후보
-				if(--numbers[back]==0) q.offer(back); //front를 세웠으므로 앞사람 인원수가 줄고, 만약 0이되면 그 학생의 순서이므로 큐에 넣는다
-			}
-		}
-		
-		
-		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); //학생수
+        int M = Integer.parseInt(st.nextToken()); //키를 비교한 횟수
+
+        ArrayList<List<Integer>> list = new ArrayList<>();
+        int[] indegree = new int[N+1];
+        for(int i=0; i<=N; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int front = Integer.parseInt(st.nextToken());
+            int back = Integer.parseInt(st.nextToken());
+
+            list.get(front).add(back);
+            indegree[back] ++; //간선의 갯수 셋팅
+        }
+
+        System.out.println(topologicalSort(N, list, indegree));
+
+
+    }
+
+    private static String topologicalSort(int N, ArrayList<List<Integer>> list, int[] indegree) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> q = new ArrayDeque<>();
+
+        //큐 셋팅
+        for(int i=1; i<=N; i++) if(indegree[i]==0) q.offer(i);
+
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            sb.append(curr).append(' ');
+
+            for(int i : list.get(curr)){
+                if(-- indegree[i] == 0) q.offer(i);
+            }
+        }
+
+        return sb.toString();
+        
+    }
+
+
 }
