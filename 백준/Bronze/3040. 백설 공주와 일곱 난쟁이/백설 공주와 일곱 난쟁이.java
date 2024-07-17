65e6cd47736cda414ad33277ed234a03bc88a870
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-18 01:22:43 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,45 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-/* 백설공주와 일곱난쟁이
- * 
- * 9C7을 구한뒤 해당 조합의 합이 100인지 체크하여 리턴한다
- * 
+/** 백설공주와 일곱난쟁이
+ * 각 난쟁이 모자에 100보다 작은 정수가 적혀있고, 그 합이 100이다
+ * 이제 진짜 난쟁이 일곱명만 찾아보자. 즉 아홉개 수중 합이 100이 되는 일곱개 수를 찾자
+ * 풀이 : 조합으로 찾는게 가장 먼저 떠오르네요..
  */
 public class Main {
-	static int[] arr, selected;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		arr = new int[9];
-		selected = new int[7];
-		for(int i=0; i<9; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		solution(0,0);
-		System.out.println(sb);
-	}
-	private static void solution(int cnt, int start) {// cnt = 현재 선택된 조합 갯수 //start=시작인덱스
-		if(cnt == 7) { //7번째는 들어올일 없음
-			int result = 0;
-			for(int s : selected) {
-				result+=s;
-				if(result>100) return;
-			}
-			
-			if(result==100) {
-				for(int s : selected) sb.append(s).append("\n");
-			}
-			return;
-		}
-		
-		for(int i=start; i<9; i++) { //이전에 선택된 숫자 이후의 숫자만 찾는다 = 조합
-			selected[cnt] = arr[i];
-			solution(cnt+1,i+1);
-		}
-	}
+    static int[] arr = new int[9];
+    static int[] comb = new int[7];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i=0; i<9; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        getCombination(0,0);
+    }
+
+    private static void getCombination(int cnt, int start) {
+        if(cnt == 7){
+            int sum = 0;
+            //현재 조합에서 모든 난쟁이 합이 100이하인지 찾기
+            for(int i : comb) sum += i;
+            if(sum == 100) {
+                for(int c : comb) System.out.println(c);
+            }
+
+            return;
+        }
+
+        for(int i=start; i<9; i++){
+            comb[cnt] = arr[i];
+            getCombination(cnt+1, i+1);
+        }
+    }
 }
