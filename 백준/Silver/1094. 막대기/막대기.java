8ede9ab8d8b1af8b9673578618a3db38c09372c5
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-19 00:42:43 +0900

    [Silver V] Title: 막대기, Time: 76 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -3,27 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /** 막대기
- * 64cm의 막대기를 Xcm가 되도록 자른다.
- * 즉, 절반씩 나누면서 길이를 비교하다가 기준값보다 크면 반 버리고,
- * 기준값 X보다 작으면 반은 막대기의 부품이 되고, 반은 계속 자르는거임
- * 따라서 부품이된만큼 X는 줄어들고, 나머지 반을 또 이전처럼 비교하며 더 작게 만듦
- * 비트마스킹으로도 풀수있대!
- * */
+ * 64cm막대기를 Xcm로 만든다.
+ * (1)막대길이를 모두 더해 x보다 크면 작은걸 반갈
+ * (2) 절반중 하나를 빼고 남은 막대 합이 X이상이면 빼둔걸 버린다.
+ * (3) 반복
+ *
+ * (풀이) 반복문 돌면서 stick이 X보다 길면 2로 나눈다.
+ *
+ *          X=23인데 stick이 32라면, 반으로 자른 후 남은 막대가 16으로 X보다 작게 된다.
+ *          즉 이 시점부터는 16+16중 버리는것이 없다. 이제 둘중 하나만(더 짧은것만) 돌릴것이므로
+ *          아예 목표치를 줄여버린다. X=7이다. 이상태에서 하나남은 16은 또 잘라야하고
+ *          8이 된다. 그럼 다음에 4+4가 되어 x=3이 되며, 4 하나 남은걸 또 돌린다
+ *          2+2가 되고, x=1이 된다. 2를 또 반으로 자르면 1로 드디어 완성
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
+        int stick = 64;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int x = Integer.parseInt(br.readLine());
-        int stick = 64;
-        int count = 0;
+        int cnt = 0;
 
-        while(x > 0){
-            if(stick>x) stick /= 2; //막대기가 x보다 크면 반으로 계속 나눔
-            else {
-                //막대기가 x보다 작으면 목표치 x를 채우도록 (즉 빼가면서 0이되도록) 하자
-                x -= stick;
-                count++;
+        while(x>0){
+            if(stick > x) stick /= 2; //절반이 x보다 크면 걍 하나는 버려버림
+            else{ //절반이 x보다 작다면
+                cnt++;
+                x -= stick; //목표stick에 하나 붙인다고 침 (어차피 작은스틱만 반복하니까)
             }
         }
-        System.out.println(count);
+
+        System.out.println(cnt);
     }
 }
