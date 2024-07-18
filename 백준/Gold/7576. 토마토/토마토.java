b1bd21017634fd6b900aa5e969e924fce8b5fe7c
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-18 22:54:38 +0900

    [Gold V] Title: 토마토, Time: 604 ms, Memory: 142820 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,63 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-// x,y좌표와 길이를 Node에서 관리한다.
-class Node{
-	int x,y,len;
-	Node(int x, int y, int len){
-		this.x = x;
-		this.y = y;
-		this.len = len;
-	}
-}
-
-
+/** 토마토
+ * 토마토를 2차원 격자에 넣어 보관한다.
+ * 익은 토마토 근처(상하좌우)의 토마토는 익는다.
+ * 며칠이 지나면 모든 토마토가 익는지 최소 일수를 구해라!
+ *
+ * 풀이 : 익은 토마토 배열을 만들고, 매일(반복문) 그 토마토들의 사방을 업데이트해준다.
+ * 안익은 토마토 갯수에서 -1을 하다가 0이되면 종료하면 될듯?,., 완전탐색에 가까운데..
+ */
 public class Main {
-	
-	static int[] dx = {0,0,1,-1};
-	static int[] dy = {1,-1,0,0};
-	static int[][] arr;
-	static Queue<Node> q = new LinkedList<Node>();
-	static int tomato=0; //익은토마토+안익은 토마토 갯수 카운트
-	static int result=0; //모든 토마토가 익는데 걸리는 일수 카운트 
-	
-	//토마토가 익는 일수를 계산한다. 최단경로를 구하는것이므로 BFS를 사용
-	public static void solution() {
-		while(!q.isEmpty()) {
-			Node node = q.poll();
-			tomato--;
-			for(int i=0; i<4; i++) {
-				int nx = node.x+dx[i];
-				int ny = node.y+dy[i];
-				if(nx>=0 && ny>=0 && nx<arr.length && ny<arr[0].length && arr[nx][ny]==0) {
-					arr[nx][ny] = 1;
-					q.offer(new Node(nx,ny,node.len+1)); //len일차에 익은 토마토에 영향 받은것이므로 len+1일차로 표시
-					result = node.len+1;
-				}
-			}
-		}
-		return;
-	}
-	
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		arr = new int[n][m];
-		tomato = n*m;
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				arr[i][j]=sc.nextInt();
-				if(arr[i][j]==1) {
-					q.offer(new Node(i,j,0));
-				} else if(arr[i][j] == -1) tomato--; //익은 토마토 + 안익은 토마토의 갯수 셋팅
-			}
-		}
-		
-		solution();
-		
-		if(tomato==0) System.out.println(result); //모든 토마토가 익었으면 일수 출력 
-		else System.out.println(-1); //안익은 토마토가 있다면 -1을 출력
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken()); //열
+        int N = Integer.parseInt(st.nextToken()); //행
+        int[][] map = new int[N][M];
+        List<int[]> redTomato = new ArrayList<>();
+        int totalNotRedCnt = 0;
+        //N열 M행, 1은익은토마토 0은안익은토마토 -1은빈칸
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1) redTomato.add(new int[]{i,j});
+                else if(map[i][j] == 0) totalNotRedCnt++;
+            }
+        }
+
+        //익은 토마토를 기준으로 매일을 사방탐색으로 기록한다
+        //하루치 전이를 끝낸 토마토는 이후 반복부턴 돌지않음
+        int day = solution(totalNotRedCnt, redTomato,N,M,map);
+        System.out.println(day);
+    }
+
+    private static int solution(int totalNotRedCnt, List<int[]> redTomato, int N, int M, int[][] map) {
+        int[] dx = new int[]{0,1,0,-1};
+        int[] dy = new int[]{1,0,-1,0};
+        int day = 0;
+        if(totalNotRedCnt == 0) return 0;
+        while(!redTomato.isEmpty()){
+            day++;
+            ArrayList<int[]> newTomato = new ArrayList<>();
+            for(int[] tomato : redTomato){
+                for(int i=0; i<4; i++){
+                    int nextX = tomato[0] + dx[i];
+                    int nextY = tomato[1] + dy[i];
+                    if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
+                    if(map[nextX][nextY] == 0) { //안익은 토마토가 있을때만
+                        map[nextX][nextY] = 1;
+                        newTomato.add(new int[]{nextX, nextY});
+                        totalNotRedCnt --;
+                        if(totalNotRedCnt == 0) return day;
+                    }
+                }
+            }
+            redTomato = newTomato;
+        }
+        return -1;
+    }
+
 }
