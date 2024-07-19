23f825fecccebf010efc725b0f3d10764cde16e8
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-20 04:21:04 +0900

    [Bronze II] Title: OX퀴즈, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,12 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /** OX퀴즈
- * OOXXOXXOOO의 경우 연속된 O의 갯수를 그자리에 넣어 계산
- * 즉 1+2+1+1+2+3 = 10점임
- * 풀이 : 반복문 돌면서 누적O갯수를 변수에 따로 저장하면서 sum을 동시에 계산
+ * O의 각 자리에서의 연속된 횟수를 모두 더한다.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,14 +11,13 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<T; i++){
-            int sum = 0;
-            int cnt = 0;
             char[] arr = br.readLine().toCharArray();
+            int cnt = 0, sum = 0;
             for(char c : arr){
-                if(c=='O') {
-                    cnt += 1;
+                if(c=='O'){
+                    cnt++;
                     sum += cnt;
-                }else cnt = 0;
+                } else cnt = 0;
             }
             sb.append(sum).append("\n");
         }
