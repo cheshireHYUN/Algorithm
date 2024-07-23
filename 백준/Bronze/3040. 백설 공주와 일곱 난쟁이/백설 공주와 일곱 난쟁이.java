8055ebc987e9d7b87152849d2e01a814040cd175
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-23 21:49:28 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 76 ms, Memory: 11456 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,40 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /** 백설공주와 일곱난쟁이
- * 각 난쟁이 모자에 100보다 작은 정수가 적혀있고, 그 합이 100이다
- * 이제 진짜 난쟁이 일곱명만 찾아보자. 즉 아홉개 수중 합이 100이 되는 일곱개 수를 찾자
- * 풀이 : 조합으로 찾는게 가장 먼저 떠오르네요..
+ * 9개중 7를 뽑는 조합을 구해서 합이 100인것을 출력
  */
 public class Main {
-    static int[] arr = new int[9];
-    static int[] comb = new int[7];
+    static int[] arr, selected;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for(int i=0; i<9; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+        arr = new int[9];
+        selected = new int[7];
+        for(int i=0; i<9; i++) arr[i] = Integer.parseInt(br.readLine());
 
-        getCombination(0,0);
+        solution(0,0);
+        System.out.println(sb);
     }
 
-    private static void getCombination(int cnt, int start) {
+    static StringBuilder sb = new StringBuilder();
+    private static void solution(int cnt, int start){
         if(cnt == 7){
             int sum = 0;
-            //현재 조합에서 모든 난쟁이 합이 100이하인지 찾기
-            for(int i : comb) sum += i;
+            for(int i : selected) sum += i;
             if(sum == 100) {
-                for(int c : comb) System.out.println(c);
+                for(int i : selected) sb.append(i).append("\n");
             }
-
             return;
         }
-
         for(int i=start; i<9; i++){
-            comb[cnt] = arr[i];
-            getCombination(cnt+1, i+1);
+            selected[cnt] = arr[i];
+            solution(cnt+1, i+1);
         }
     }
 }
