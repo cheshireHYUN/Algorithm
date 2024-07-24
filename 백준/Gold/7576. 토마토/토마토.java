44f6277b2c5fdd4937a2fc36d8dbfa60bfcae413
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-25 05:05:52 +0900

    [Gold V] Title: 토마토, Time: 552 ms, Memory: 101860 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,67 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 /** 토마토
- * 토마토를 2차원 격자에 넣어 보관한다.
- * 익은 토마토 근처(상하좌우)의 토마토는 익는다.
- * 며칠이 지나면 모든 토마토가 익는지 최소 일수를 구해라!
- *
- * 풀이 : 익은 토마토 배열을 만들고, 매일(반복문) 그 토마토들의 사방을 업데이트해준다.
- * 안익은 토마토 갯수에서 -1을 하다가 0이되면 종료하면 될듯?,., 완전탐색에 가까운데..
- * => BFS와 유사하게 전개했다! 이젠 레벨별 BFS로 전환해보자.
+ * 사방으로 토마토가 익음. 익은거1 안익은거0 빈칸-1
+ * 풀이 : 안익은 토마토 갯수가 1이상이면 시뮬레이션 진행
+ * 사방탐색하며 체크배열 만들어서 안익은거 0이면 종료
+ * BFS하면서 출발점 큐를 돌리면서 몇레벨인지 탐색
  */
 public class Main {
+    static int[][] map;
+    static boolean[][] check;
+    static int N,M,green,red;
+    static Queue<int[]> q = new ArrayDeque<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken()); //열
-        int N = Integer.parseInt(st.nextToken()); //행
-        int[][] map = new int[N][M];
-        Queue<int[]> q = new ArrayDeque<>();
-        int totalNotRedCnt = 0;
-        //N열 M행, 1은익은토마토 0은안익은토마토 -1은빈칸
-        for (int i = 0; i < N; i++) {
+        M = Integer.parseInt(st.nextToken()); //가로칸 수 = 열
+        N = Integer.parseInt(st.nextToken()); //세로칸 수 = 행
+        map = new int[N][M];
+        check = new boolean[N][M];
+        for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for(int j=0; j<M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) q.offer(new int[]{i, j});
-                else if (map[i][j] == 0) totalNotRedCnt++;
+                if(map[i][j] == 1) {
+                    q.offer(new int[]{i,j});
+                    check[i][j] = true;
+                }
+                else if(map[i][j] == 0) green++;
             }
         }
-
-        //익은 토마토를 기준으로 매일을 사방탐색으로 기록한다
-        //하루치 전이를 끝낸 토마토는 이후 반복부턴 돌지않음
-        int day = solution(totalNotRedCnt, q, N, M, map);
-        System.out.println(day);
+        if(green == 0) System.out.println(0);
+        else  System.out.println(solution());
     }
 
-    private static int solution(int totalNotRedCnt, Queue<int[]> q, int N, int M, int[][] map) {
-        int[] dx = new int[]{0, 1, 0, -1};
-        int[] dy = new int[]{1, 0, -1, 0};
+    private static int solution(){
+        int[] dx = {0,0,1,-1};
+        int[] dy = {1,-1,0,0};
         int day = 0;
-        if (totalNotRedCnt == 0) return 0;
-
-        //레벨별 BFS탐색으로 변환해보자.
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             day++;
-            int len = q.size();
-            for (int l = 0; l < len; l++) {
-                int[] redTomato = q.poll();
-                for (int i = 0; i < 4; i++) {
-                    int nextX = redTomato[0] + dx[i];
-                    int nextY = redTomato[1] + dy[i];
-
-                    if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) continue;
-                    if (map[nextX][nextY] == 0) { //안익은 토마토가 있을때만
-                        map[nextX][nextY] = 1;
-                        q.offer(new int[]{nextX, nextY});
-                        totalNotRedCnt--;
-                        if (totalNotRedCnt == 0) return day;
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int[] curr = q.poll();
+                for(int j=0; j<4; j++){
+                    int x = curr[0]+dx[j];
+                    int y = curr[1]+dy[j];
+                    if(x<0 || y<0 || x>=N || y>=M || check[x][y]) continue;
+                    if(map[x][y] == 0){
+                        green --;
+                        q.offer(new int[]{x,y});
+                        check[x][y] = true;
                     }
                 }
             }
+            if(green == 0) return day;
         }
         return -1;
     }
