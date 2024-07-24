e9830287617aa7e01ee8ea7ffe495698a93585b3
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-25 05:22:16 +0900

    [Silver V] Title: 숫자 카드, Time: 888 ms, Memory: 174784 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,30 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 /** 숫자카드
- * 정수하나가 적혀진 카드다. 숫자카드 N개를 가지고 있을때 정수 M개가 주어진 상황에서
- * 이 숫자 카드를 상근이가 갖고있을지 구하자.
- * (풀이) contains()를 쓰려고 했는데 이거 시간복잡도 N이여서 MXN으로 시간초과남
- *
+ * 상근이가 N개의 숫자카드를 가지고 있고 정수M개가 주어졌을때
+ * 이 수가 적힌 숫자카드를 상근이도 갖고있는지 구하자
+ * 풀이 : HashSet을 이용해서 contains()하나 보면 될듯 nlogn이라 3000만?
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); //상근이가 가진 숫자카드 갯수
-        StringTokenizer st = new StringTokenizer(br.readLine()); //숫자카드에 적힌 정수
-        Set<Integer> set = new HashSet<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
         for(int i=0; i<N; i++) set.add(Integer.parseInt(st.nextToken()));
-
-        StringBuilder sb = new StringBuilder();
         int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<M; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if(set.contains(num)) sb.append(1).append(" ");
+            if(set.contains(Integer.parseInt(st.nextToken()))) sb.append(1).append(" ");
             else sb.append(0).append(" ");
         }
         System.out.println(sb);
+
     }
 }
