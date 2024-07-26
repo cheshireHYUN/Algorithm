c75568b5ae03272204a5548b11e1b68f48782360
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-27 02:47:56 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 21060 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,39 +4,35 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/** N과 M(1)
- * 자연수 N과 M이 주어졌을때 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하라.
- * (1부터 N까지 자연수 중에서 중복없이 M개를 고른 수열)
- * 풀이 : 그냥 1부터N까지 수중에서 M개를 뽑는 수열(중복X)을 만들자. => 중복 확인 배열
+/** N과 M
+ * 1~N까지 자연수 중 중복없이 M개를 고른 수열
  */
 public class Main {
     static int N,M;
-    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        select = new int[M];
+        selected = new int[M];
         check = new boolean[N+1];
-        permutation(0);
+        solution(0);
         System.out.println(sb);
     }
-
-    static int[] select;
+    static StringBuilder sb = new StringBuilder();
+    static int[] selected;
     static boolean[] check;
-    private static void permutation(int cnt) {
+    static void solution(int cnt){
         if(cnt == M){
-            for(int i : select) sb.append(i).append(" ");
+            for(int i: selected) sb.append(i).append(" ");
             sb.append("\n");
             return;
         }
         for(int i=1; i<=N; i++){
             if(check[i]) continue;
-
-            select[cnt] = i;
             check[i] = true;
-            permutation(cnt+1);
+            selected[cnt] = i;
+            solution(cnt+1);
             check[i] = false;
         }
     }
