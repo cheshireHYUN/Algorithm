6b639ca52561744d792f150ee8b93abf9eda7fec
Cheshire <seohyun@chungbuk.ac.kr>
2024-07-28 03:11:42 +0900

    [Silver V] Title: 뒤집기, Time: 84 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,33 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
-import java.util.Arrays;
 
 /** 뒤집기
- * 0과 1로만 이뤄진 문자열 S의 숫자를 전부 같게할것임
- * 연속된 하나이상의 숫자를 잡고 모두 뒤집는다. 최소 행동 횟수를 구하라
- * 풀이 : 그리디래요
- * 쉽게 생각해보면... 결국 1100처럼 서로다른 숫자를 묶어서 뒤집으면 결국 현상유지밖에 안됨
- * 그렇다면? 0세트와 1세트중에서 더 작은걸 다 뒤집으면 된다.
+ * 다솜이는 0과 1로 이루어진 문자열 S를 갖고있고, 모두 같게 할거다.
+ * 0001100 => 1
+ * 11111 => 0
+ * 0000001 => 1
+ * 11001100110011000001 => 4
+ * 11101101 => 2
+ * 풀이 : 결국 더 적은 묶음을 선택하면 되는것이다. 어차피 한번에 뒤집을 수 있으니... 1과 0의 블록 갯수를 세면됨
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int[] arr = new int[str.length()];
-        for(int i=0; i<str.length(); i++) arr[i] = Integer.parseInt(str.substring(i,i+1));
+        char[] arr = br.readLine().toCharArray();
 
-        int cnt1 = 0, cnt2 = 0, pre=arr[0];
-        for(int i=0; i<arr.length; i++){
-            if(i==0 || pre!=arr[i]){
-                pre=arr[i];
-                if(arr[i] == 1) cnt2++;
+        char pre = arr[0];
+        int cnt0=0, cnt1=0;
+
+        if(pre == '0') cnt0++;
+        else cnt1++;
+
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] != arr[i-1]) {
+                pre = arr[i];
+                if(arr[i] == '0') cnt0++;
                 else cnt1++;
             }
         }
 
-        int result = Math.min(cnt1, cnt2);
+        int result = Math.min(cnt0, cnt1);
         System.out.println(result);
     }
 }
