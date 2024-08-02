99f29e265165d65fc9e1e2e53793abc7e264afe2
Cheshire <seohyun@chungbuk.ac.kr>
2024-08-03 02:26:44 +0900

    [Silver V] Title: 뒤집기, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -3,34 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /** 뒤집기
- * 다솜이는 0과 1로 이루어진 문자열 S를 갖고있고, 모두 같게 할거다.
- * 0001100 => 1
- * 11111 => 0
- * 0000001 => 1
- * 11001100110011000001 => 4
- * 11101101 => 2
- * 풀이 : 결국 더 적은 묶음을 선택하면 되는것이다. 어차피 한번에 뒤집을 수 있으니... 1과 0의 블록 갯수를 세면됨
+ * 0001100은 010과 같고, 0과1중 더 작은것의 갯수만큼만 결과가 나오면됨
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] arr = br.readLine().toCharArray();
 
-        char pre = arr[0];
         int cnt0=0, cnt1=0;
-
-        if(pre == '0') cnt0++;
+        char pre=arr[0];
+        if(pre=='0') cnt0++;
         else cnt1++;
 
         for(int i=1; i<arr.length; i++){
-            if(arr[i] != arr[i-1]) {
+            if(pre!=arr[i]){
                 pre = arr[i];
-                if(arr[i] == '0') cnt0++;
+                if(pre=='0') cnt0++;
                 else cnt1++;
             }
         }
 
-        int result = Math.min(cnt0, cnt1);
+        int result = Math.min(cnt1, cnt0);
         System.out.println(result);
+
     }
 }
