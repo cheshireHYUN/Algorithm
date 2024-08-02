9c066e0bb8af3993c8a3c9fc6af2871d58a83848
Cheshire <seohyun@chungbuk.ac.kr>
2024-08-03 03:21:38 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 796 ms, Memory: 283024 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -3,60 +3,62 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/** 어떤나라에서 1번부터 N번까지 도시와 M개의 단방향 도로가 존재하며 도로의 거리는 1이다.
- * 특정 도시 X로부터 출발하여 도착하는 도시중 최단거리가 K인 도시번호를 출력하라.
- * (풀이) BFS를 사용하여 레벨별탐색으로 K번째 단계의 도시를 출력하면 될듯
+/** 특정 거리의 도시찾기
+ * 1번부터 N번까지 도시와 M개의 단방향 도로가 존재한다. 모든 거리는 1이다.
+ * 특정 도시 X로부터 도달하는 도시중 최단거리가 K인 도시 오름차순 출력
+ * BFS를 레벨별 탐색하면됨 체크 꼭하고
  */
 public class Main {
-    static List<ArrayList<Integer>> list;
     static int N,M,K,X;
+    static List<ArrayList<Integer>> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken()); //도시의 갯수
-        M = Integer.parseInt(st.nextToken()); //도로의 갯수
-        K = Integer.parseInt(st.nextToken()); //목표 거리
-        X = Integer.parseInt(st.nextToken()); //출발도시 번호
+        N = Integer.parseInt(st.nextToken()); //도시갯수
+        M = Integer.parseInt(st.nextToken()); //도로갯수
+        K = Integer.parseInt(st.nextToken()); //최단거리
+        X = Integer.parseInt(st.nextToken()); //출발도시번호
 
-        list = new ArrayList<>();
         for(int i=0; i<N+1; i++) list.add(new ArrayList<>());
-        for(int i=0; i<M; i++){
+        for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
-            list.get(Integer.parseInt(st.nextToken()))
-                    .add(Integer.parseInt(st.nextToken()));
+            list.get(Integer.parseInt(st.nextToken())).add(Integer.parseInt(st.nextToken()));
         }
 
         List<Integer> result = bfs();
-        if(result.size() == 0) System.out.println(-1);
-        else {
-            Collections.sort(result);
-            for(int i : result) System.out.println(i);
-        }
-
+        Collections.sort(result);
+        for(int i: result) sb.append(i).append("\n");
+        System.out.println(sb);
     }
 
-    private static List<Integer> bfs() {
-        List<Integer> result = new ArrayList<>();
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(X);
+    static StringBuilder sb = new StringBuilder();
+    private static List<Integer> bfs(){
         boolean[] check = new boolean[N+1];
+        Queue<Integer> q = new ArrayDeque();
+        q.offer(X);
         check[X] = true;
         int level = 0;
         while(!q.isEmpty()){
+            int size = q.size();
             level++;
-            int len = q.size();
-            for(int i=0; i<len; i++){
+            for(int i=0; i<size; i++){
                 int curr = q.poll();
-                for(int li : list.get(curr)){
-                    if(!check[li]){
-                        check[li] = true;
-                        q.offer(li);
-                        if(level == K) result.add(li);
+                for(int a : list.get(curr)){
+                    if(!check[a]) {
+                        check[a] = true;
+                        q.offer(a);
                     }
                 }
             }
             if(level == K) break;
         }
+
+        List<Integer> result = new ArrayList<>();
+        if(q.isEmpty()) result.add(-1);
+        else {
+            while(!q.isEmpty()) result.add(q.poll());
+        }
+
         return result;
     }
 }
