fd97357a49ab88e325b4759d41764ebf256190f4
Cheshire <seohyun@chungbuk.ac.kr>
2024-08-03 02:44:06 +0900

    [Silver III] Title: N과 M (1), Time: 160 ms, Memory: 20548 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -5,35 +5,35 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /** N과 M
- * 1~N까지 자연수 중 중복없이 M개를 고른 수열
+ *  1에서 N까지 수중에서 중복없이 M개를 고른 수열
  */
 public class Main {
-    static int N,M;
+    static int N,M, selected[];
+    static boolean[] check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        selected = new int[M];
         check = new boolean[N+1];
-        solution(0);
+        selected = new int[M];
+        permutation(0);
         System.out.println(sb);
     }
+
     static StringBuilder sb = new StringBuilder();
-    static int[] selected;
-    static boolean[] check;
-    static void solution(int cnt){
+    private static void permutation(int cnt){
         if(cnt == M){
-            for(int i: selected) sb.append(i).append(" ");
+            for(int i : selected) sb.append(i).append(" ");
             sb.append("\n");
             return;
         }
         for(int i=1; i<=N; i++){
             if(check[i]) continue;
             check[i] = true;
-            selected[cnt] = i;
-            solution(cnt+1);
-            check[i] = false;
+            selected[cnt]=i;
+            permutation(cnt+1);
+            check[i]=false;
         }
     }
 }
