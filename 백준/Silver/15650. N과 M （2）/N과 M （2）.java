93c77c555963e7a45b0e6b155ebf1058bef12fe5
Cheshire <seohyun@chungbuk.ac.kr>
2024-08-23 17:05:52 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,39 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-/**
- * N과 M이 주어졌을때 조건을 만족하는 길이가 M인 수열
- * 조건 1. 1부터 N까지 자연수중 중복없이 M개를 고른 수열
- * 조건 2. 고른 수열은 오름차순이어야 함
- *
+/** N과 M(2)
+ * 1~N까지 자연수중에서 중복없이 M개 고른다
+ * 고른 수열은 오름차순이어야한다
+ * N개중에서 M개를 뽑는 조합을 구하고 오름차순 정렬하면 되지 않을까>
  */
 public class Main {
-	static int M,N;
-	static int[] numbers;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		numbers = new int[M];
-		
-		solution(0, 1);
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        solution(0,1);
+        System.out.println(sb);
+    }
 
-	private static void solution(int cnt, int idx) { //현재까지 조합 원소 갯수, 조합시도할 시작인덱스
-		// 1부터 n까지 중복없이 m개를 고른다..
-		if(cnt == M) {
-			for(int n : numbers) sb.append(n+" ");
-			sb.append("\n");
-			return;
-		}
-		for(int i=idx; i<=N; i++) {
-			numbers[cnt] = i;
-			solution(cnt+1, i+1);
-		}
-	}
+    private static int arr[], N, M;
+    private static StringBuilder sb = new StringBuilder();
+    private static void solution(int cnt, int start) {
+        if(cnt == M) {
+            for(int i : arr) sb.append(i).append(' ');
+            sb.append('\n');
+            return;
+        }
 
+        for(int i=start; i<N+1; i++){
+            arr[cnt] = i;
+            solution(cnt+1, i+1);
+        }
+    }
 }
