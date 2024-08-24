03f0167d0df76ed2a575caab6208c4d37a53bfef
Cheshire <seohyun@chungbuk.ac.kr>
2024-08-24 16:24:51 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 392 ms, Memory: 115140 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,24 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /** 연결요소의 갯수
- * 방향없는 그래프에서 연결요소의 갯수를 찾는 프로그램을 작성하라.
- * DFS나 BFS로 각자 세도 되고, 유니온앤파인드를 해도 되겠는걸
+ * 무방향 그래프 주어졌을때 연결요소의 갯수를 구하라.
+ * 풀이 : Union&find 또는 BFS. 나는 유니온앤 파인드 써야지 효율좋우니까
  */
 public class Main {
-    static int[] parents;
+    private static int N,M,parents[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //정점
-        int M = Integer.parseInt(st.nextToken()); //간선
+        N = Integer.parseInt(st.nextToken()); //정점의 갯수
+        M = Integer.parseInt(st.nextToken()); //간선의 갯수
         parents = new int[N+1];
         for(int i=1; i<N+1; i++) parents[i] = i;
-
         for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -26,22 +26,23 @@ public class Main {
             union(a,b);
         }
 
-        HashSet<Integer> set = new HashSet<>();
+        //혹시 업데이트 안됐을 parents를 위해 find한번 더
+        HashSet<Integer> set = new HashSet<Integer>();
         for(int i=1; i<N+1; i++) set.add(find(parents[i]));
         System.out.println(set.size());
     }
 
-    static boolean union(int a, int b){
-        int rootA = find(a);
-        int rootB = find(b);
+    private static boolean union(int a, int b){
+        int aRoot = find(a);
+        int bRoot = find(b);
 
-        if(rootA == rootB) return false;
-        else parents[rootB] = rootA;
+        if(aRoot == bRoot) return false;
+        parents[bRoot] = aRoot;
         return true;
     }
 
-    static int find(int v){
+    private static int find(int v){
         if(parents[v] == v) return v;
-        else return parents[v] = find(parents[v]);
+        return parents[v] = find(parents[v]);
     }
 }
