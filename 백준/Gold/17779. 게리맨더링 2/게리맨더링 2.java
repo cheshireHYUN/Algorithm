e3aabe4f356f51da689c6792f7137b4cc771372c
Cheshire <seohyun@chungbuk.ac.kr>
2025-02-20 22:07:46 +0900

    [Gold III] Title: 게리맨더링 2, Time: 232 ms, Memory: 29628 KB -BaekjoonHub

--- 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
+++ 백준/Gold/17779. 게리맨더링 2/게리맨더링 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /** 게리멘더링 2
@@ -15,11 +16,17 @@ import java.util.StringTokenizer;
  * 우선 선거구를 나누는 각 방법들을 시도해야한다.
  * 필요한건 기준점 (x,y)와 d1,d2인데,
  * 1<=d1,d2 // 1<=x<x+d1+d2<=N // 1<=y-d1<y<y+d2<=N 이라는 조건을 통과해야함.
- * 위 조건을 통과하는 기준점과 d1,d2를 이용해 경계선을 그어 선거구를 확정하고, 각 구역별 인구값을 구해 답을 구한다.
+ * 위 조건을 통과하는 기준점과 d1,d2를 이용해 (완전탐색)
+ * 경계선을 그어 선거구를 확정하고, 각 구역별 인구값을 구해 답을 구한다. (구현)
+ * 경계구역 밖을 쉽게 탐색하기위해 맵의 가생이부터 탐색하는것이 키포인트였다.
+ *
+ * [개선]
+ * 각 경우마다 최대최소 인구수를 측정할때 배열에 넣은 뒤 sort해버리면 쉽게 구할 수 있다.
+ * 또한 5번 선거구의 인구수는 1,2,3,4번 구역의 인구수를 전체인구수에서 빼면 쉽게 구할 수 있다.
  */
 public class Main {
     static int[][] map;
-    static int N, result=Integer.MAX_VALUE;
+    static int N, result=Integer.MAX_VALUE, total=0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -27,7 +34,10 @@ public class Main {
         StringTokenizer st;
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                total += map[i][j];
+            }
         }
 
         for(int x=0; x<N; x++){
@@ -57,84 +67,62 @@ public class Main {
     private static void divide(int x, int y, int d1, int d2) {
         //System.out.println("[기준점] : ("+x+","+y+") // d1="+d1+", d2="+d2);
         //경계선을 계산한다 : 경계선은 모두 5번 선거구
-        int[][] dMap = new int[N][N];
+        boolean[][] dMap = new boolean[N][N];
+        int[] cntArr = new int[5];
+
         //1번
         for(int i=x, j=y; i<=x+d1 && j>=y-d1; i++, j--) {
-            dMap[i][j] = 5;
+            dMap[i][j] = true;
         }
         for(int i=0; i<x+d1; i++) {
             for(int j=0; j<=y; j++) {
-                if(dMap[i][j] == 5) break;
-                dMap[i][j] = 1;
+                if(dMap[i][j]) break;
+                cntArr[0] += map[i][j];
             }
         }
 
         //2번
         for(int i=x, j=y; i<=x+d2 && j<=y+d2; i++, j++) {
-            dMap[i][j] = 5;
+            dMap[i][j] = true;
         }
         for(int i=0; i<=x+d2; i++) {
             for(int j=N-1; j>=y+1; j--) {
-                if(dMap[i][j] == 5) break;
-                dMap[i][j] = 2;
+                if(dMap[i][j]) break;
+                cntArr[1] += map[i][j];
             }
         }
 
         //3번
         for(int i=x+d1, j=y-d1; i<=x+d1+d2 && j<=y-d1+d2; i++,j++) {
-            dMap[i][j] = 5;
+            dMap[i][j] = true;
         }
         for(int i=x+d1; i<N; i++) {
             for(int j=0; j<y-d1+d2; j++) {
-                if(dMap[i][j] == 5) break;
-                dMap[i][j] = 3;
+                if(dMap[i][j]) break;
+                cntArr[2] += map[i][j];
             }
         }
 
         //4번
         for(int i=x+d2, j=y+d2; i<=x+d2+d1 && j>=y+d2-d1; i++,j--) {
-            dMap[i][j] = 5;
+            dMap[i][j] = true;
         }
         for(int i=x+d2+1; i<N; i++) {
             for(int j=N-1; j>=y-d1+d2; j--) {
-                if(dMap[i][j] == 5) break;
-                dMap[i][j] = 4;
+                if(dMap[i][j]) break;
+                cntArr[3] += map[i][j];
             }
         }
 
-        //계산
-        int cnt1=0,cnt2=0,cnt3=0,cnt4=0,cnt5=0;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++) {
-                if(dMap[i][j]==1) cnt1 += map[i][j];
-                else if(dMap[i][j]==2) cnt2 += map[i][j];
-                else if(dMap[i][j]==3) cnt3 += map[i][j];
-                else if(dMap[i][j]==4) cnt4 += map[i][j];
-                else if(dMap[i][j]==0 || dMap[i][j]==5) cnt5 += map[i][j];
-            }
-        }
+        //5번
+        cntArr[4] = total-(cntArr[0]+cntArr[1]+cntArr[2]+cntArr[3]);
 
-        int min = getMin(cnt1,cnt2,cnt3,cnt4,cnt5);
-        int max = getMax(cnt1,cnt2,cnt3,cnt4,cnt5);
+        Arrays.sort(cntArr);
+        int min = cntArr[0];
+        int max = cntArr[4];
 
         //print(dMap);
         result = Math.min(result, max-min);
-    }
-
-    private static int getMin(int cnt1, int cnt2, int cnt3, int cnt4, int cnt5) {
-        int min = Math.min(cnt1, cnt2);
-        min = Math.min(min, cnt3);
-        min = Math.min(min, cnt4);
-        min = Math.min(min, cnt5);
-        return min;
-    }
-
-    private static int getMax(int cnt1, int cnt2, int cnt3, int cnt4, int cnt5) {
-        int max = Math.max(cnt1, cnt2);
-        max = Math.max(max, cnt3);
-        max = Math.max(max, cnt4);
-        max = Math.max(max, cnt5);
-        return max;
     }
 
     private static void print(int[][] dMap) {
