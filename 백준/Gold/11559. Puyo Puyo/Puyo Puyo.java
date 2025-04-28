7215633c5dc1e7a4f290d4a7f6e3c4a35adf264b
Cheshire <seohyun@chungbuk.ac.kr>
2025-04-28 23:42:15 +0900

    [Gold IV] Title: Puyo Puyo, Time: 184 ms, Memory: 18192 KB -BaekjoonHub

---
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/** 뿌요뿌요
+ * 1. 필드에 여러가지 색깔의 뿌요를 놓는다. 중력의 영향을 받아 아래 바닥이나 다른뿌요가 나올때까지 떨어진다.
+ * 2. 뿌요를 놓고난 후, 같은색 뿌요가 4개이상 상하좌우로 연결되어있으면 연결된 같은색 뿌요들이 한번에 없어진다(1연쇄)
+ * 3. 뿌요들이 없어지고나서 위에 다른 뿌요들이 있다면 중력의 영향을 받아 차례로 아래로 떨어진다.
+ * 4. 아래로 떨어지고나서 다시 같은색 뿌요들이 4개이상 모이면 또 터지는데, 터진 후 뿌요들이 내려오고 다시 터짐을 반복할떄 +1연쇄
+ * 5. 터질수있는 뿌요가 여러그룹이라면 동시에 터져야하고 여러그룹이 터져도 한번의 연쇄만 추가된다.
+ *
+ * 상태방의 필드가 주어졌을때, 연쇄가 몇번 연속으로 일어날지 계산하라.
+ *
+ * 풀이
+ * 1. 터질수있는 뿌요그룹을 여러개 찾는 dfs 메소드 호출
+ * visited배열을 초기화해 모든 노드에 대해 dfs 탐색 시도
+ * 만약 해당 시작점에 대한 List.size()가 4이상이면 List값을 결과에 누적, 아니면 List값 버림
+ *
+ * 2. 연결된 갯수를 찾는 dfs 메소드
+ * visited를 받아 dfs를 돌며 true처리 및 List<int[]> 추가
+ *
+ * 3. List에 저장된 좌표를 터트리는 메소드
+ * map[i][j]가 터지면, map[i][j]=map[i-1][j], map[i-1][j]=map[i-2][j].. 이런식으로 반복된다(만약 우변의값이 '.'이면 실행후 반복종료)
+ * 위에서부터 터트려야 아래도 확실히 변함에 주의.
+ *
+ * 4. 위를 반복하며 더이상 터질 뿌요가 없을때 종료
+ */
+public class Main {
+    static char[][] map = new char[12][6];
+    static int[] dx = new int[]{0,1,-1,0};
+    static int[] dy = new int[]{1,0,0,-1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i=0; i<12; i++) map[i] = br.readLine().toCharArray();
+
+        int sum = 0;
+        while(true) {
+            List<int[]> bombList = find();
+            if(bombList.isEmpty()) break;
+            bomb(bombList);
+            sum++;
+        }
+
+        System.out.println(sum);
+    }
+
+    private static void bomb(List<int[]> bombList) {
+        bombList.sort((a1, a2) -> a1[0] - a2[0]);
+        for(int[] arr : bombList) {
+            int x = arr[0];
+            int y = arr[1];
+            for(int i=x; i>=0; i--) {
+                if(i==0) map[i][y] = '.';
+                else {
+                    map[i][y] = map[i-1][y];
+                    if(map[i][y] == '.') break;
+                }
+            }
+        }
+    }
+
+    private static List<int[]> find() {
+        boolean[][] visited = new boolean[12][6];
+        List<int[]> bombList = new ArrayList<>();
+        for(int i=0; i<12; i++) {
+            for(int j=0; j<6; j++) {
+                if(visited[i][j] || map[i][j]=='.') continue;
+                List<int[]> list = new ArrayList<>();
+                dfs(list, visited, map[i][j], i, j);
+                if(list.size()>=4) bombList.addAll(list);
+            }
+        }
+        return bombList;
+    }
+
+    private static void dfs(List<int[]> list, boolean[][] visited, char ch, int x, int y) {
+        list.add(new int[]{x,y});
+        visited[x][y] = true;
+
+        for(int i=0; i<4; i++) {
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx<0 || ny<0 || nx>=12 || ny>=6 || visited[nx][ny]) continue;
+            if(ch==map[nx][ny]) dfs(list, visited, ch, nx, ny);
+        }
+    }
+
+
+}
